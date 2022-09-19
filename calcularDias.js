5db48d8c2c750fbe39bbb542665dbb1bb267e081
NIXON0220 <108483622+NIXON0220@users.noreply.github.com>
2022-09-19 11:50:52 -0500

    update code

--- calcularDias.js
+++ calcularDias.js
@@ -1,6 +1,16 @@
 function calcularDias(){
     let fechaInicio = new Date(document.getElementById("fechaInicio").value);
-    let fechaFin = document.getElementById("fechaFin").value;
-    let fechaSistema = new Date();
+    let fechaFin =new Date(document.getElementById("fechaFin").value);
+    
+    if (fechaInicio > fechaFin) {
+        alert("La fecha de inicio no puede ser mayor a la fecha de fin");
+    } else {
+        let dias = (fechaFin - fechaInicio) / (1000 * 60 * 60 * 24);
+        alert("La cantidad de dias es: " + dias);
+    }
+    
+    let resta = fechaFin.getTime() - fechaInicio.getTime();
+    alert(resta)    
     alert(fechaInicio);
+    alert(fechaFin);
 }
