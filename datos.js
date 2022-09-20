150bc7f9b9a5e15eb5bed34918698c419e298878
NIXON0220 <108483622+NIXON0220@users.noreply.github.com>
2022-09-20 08:00:52 -0500

    add datos.js

--- datos.js
+++ datos.js
@@ -6,6 +6,7 @@ function generarpdfdatos(){
     let hora = document.getElementById("hora").value;
     let fechaInicio = new Date(document.getElementById("fechaInicio").value);
     let fechaFin =new Date(document.getElementById("fechaFin").value);
+
    
         if (fechaInicio > fechaFin) {
             alert("La fecha de inicio no puede ser mayor a la fecha de fin");
@@ -14,23 +15,26 @@ function generarpdfdatos(){
             alert("La cantidad de dias es: " + dias);
         }
         
-        // let resta = fechaFin.getTime() - fechaInicio.getTime();
-        // alert(resta)
-        // alert(fechaInicio);
-        // alert(fechaFin);
-
-
     var doc = new jsPDF();
     doc.setFontSize(20);
     doc.text(20, 20, 'Nombre: ' + nombre);
     doc.text(20, 30, 'Apellido: ' + apellido);
     doc.text(20, 40, 'Cedula: ' + cedula);
     doc.text(20, 50, 'Motivo: ' + motivo);
-    doc.text(20, 60, 'Hora: ' + hora);
+    doc.text(20, 60, 'Hora de entrada: ' + hora);
     doc.text(20, 70, 'Fecha Inicio: '+"\n" + fechaInicio);
     doc.text(20, 90, 'Fecha Fin: ' + "\n" + fechaFin); 
     doc.save('factura.pdf');
     }
+
+
+    // let resta = fechaFin.getTime() - fechaInicio.getTime();
+    // alert(resta)
+    // alert(fechaInicio);
+    // alert(fechaFin);
+
+        
+
 
 
    
